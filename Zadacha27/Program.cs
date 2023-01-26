f2a72ecd9171f27caae1a28c1aa42532f74b33af
Nadya <Nadi-lv@yandex.ru>
2023-01-26 23:44:24 +0400

    Zadacha27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,21 @@
+﻿int Digit(int n)
+{
+    n = Math.Abs(n);
+    int sum = 0;
+
+    while (n  >= 1) {
+        sum = sum + (n % 10);
+        n = n / 10;
+            }
+
+    return sum;
+}
+Console.WriteLine("Программа вычисления сумму цифр в числе");
+Console.WriteLine("Введите число n: ");
+int.TryParse(Console.ReadLine(), out int n);
+
+
+int sum = Digit(n);
+Console.WriteLine($"Результат: {sum}");
+
+
