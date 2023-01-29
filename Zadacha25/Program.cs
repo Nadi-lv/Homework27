f19ec9d85d4785094355ada0baa1929004dccee2
Nadya <Nadi-lv@yandex.ru>
2023-01-29 20:33:13 +0400

    Zadacha25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,29 @@
+﻿int exponent(int a, int b)
+{
+    if (b<1) 
+        return -1;
+
+    int result = 1;
+
+    int counter = 1;
+
+    while (counter <= b) {
+        result = result * a;
+        counter++;
+    }
+
+    return result;
+}
+Console.WriteLine("Программа числа а в степень b");
+Console.WriteLine("Введите число a: ");
+int.TryParse(Console.ReadLine(), out int a);
+Console.WriteLine("Введите число b: ");
+int.TryParse(Console.ReadLine(), out int b);
+
+if (b<1) {
+    Console.WriteLine("Степень должна быть натуральная");
+    return;
+}
+
+int result = exponent(a,b);
+Console.WriteLine($"Результат: {result}");
